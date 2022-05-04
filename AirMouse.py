166632fd0d624315e41a7588e440dfbade357984
ModDR6 <mkhal027@fiu.edu>
2022-05-04 02:09:16 +0000

    Added Coordinate Extractions

--- AirMouse.py
+++ AirMouse.py
@@ -1,3 +1,4 @@
+from turtle import left
 import mediapipe as mp
 import cv2
 import math
@@ -96,18 +97,9 @@ class AirMouse():
                                                     self.mp_drawing_styles.get_default_hand_connections_style())
         return frame
 
-
-    def get_handedness(self, index, results):
-        text = None
-        hands = results.multi_handedness
-        label = hands[index].classification[0].label
-        score = hands[index].classification[0].score
-        text = f'{label} {round(score,2)}'
-        
-        return text
-
     def start(self):
         vcap = cv2.VideoCapture(0)
+
         with self.hands as hands:
             #Start video capture
             while vcap.isOpened():
@@ -121,19 +113,30 @@ class AirMouse():
                 #Draw Logic
                 if results.multi_hand_landmarks:
                     for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
-                        self.mp_drawing.draw_landmarks(frame, hand_landmarks,  self.mp_hands.HAND_CONNECTIONS,  
-                        self.mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=6, circle_radius=1),  
-                        self.mp_drawing.DrawingSpec(color=(255, 200, 0), thickness=2, circle_radius=1)
-                        )
+
+                        #Draw Landmarks
+                        self.drawHands(frame, hand_landmarks)
 
                         #Extract Wrist Coordinates
                         x_idx = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST].x * frame.shape[1]
                         y_idx = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST].y * frame.shape[0]
-                    
+
+                        #Determine Handedness
                         handedness = self.get_handedness(idx, results)
 
+                        #Extract Coordinates
+                        right_coords, left_coords = self.extract_coordinates(hand_landmarks.landmark, handedness, frame)
+
+
+                        if right_coords:
+                            wrist_land = right_coords['PINKY_MCP']
+                        
+                        if left_coords:
+                            wrist_land = left_coords['PINKY_MCP']
+
+
                         if handedness:
-                            cv2.putText(frame, handedness, (int(x_idx), int(y_idx+20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (106, 206, 141), 2, cv2.LINE_AA)
+                            cv2.putText(frame, handedness, wrist_land, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (106, 206, 141), 2, cv2.LINE_AA)
 
 
                 # self.draw_hand(results, frame)
@@ -143,7 +146,59 @@ class AirMouse():
 
                 if cv2.waitKey(5) & 0xFF == 27:
                     break
+
             vcap.release()
+
+    def drawHands(self, frame, hand_landmarks):
+        self.mp_drawing.draw_landmarks(
+            frame, hand_landmarks,  self.mp_hands.HAND_CONNECTIONS,  
+            self.mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=6, circle_radius=1),  
+            self.mp_drawing.DrawingSpec(color=(255, 200, 0), thickness=2, circle_radius=1)
+        )
+
+    def get_handedness(self, index, results):
+        hands = results.multi_handedness
+        label = hands[index].classification[0].label
+        score = hands[index].classification[0].score
+        return label
+
+    def extract_coordinates(self, landmarks, handedness, frame):
+        id_dict = {
+        0:'WRIST', 
+        1:'THUMB_CMC', 
+        2:'THUMB_MCP', 
+        3: 'THUMB_IP', 
+        4: 'THUMB_TIP', 
+        5: 'INDEX_FINGER_MCP', 
+        6: 'INDEX_FINGER_PIP', 
+        7: 'INDEX_FINGER_DIP', 
+        8: 'INDEX_FINGER_TIP', 
+        9: 'MIDDLE_FINGER_MCP', 
+        10: 'MIDDLE_FINGER_PIP', 
+        11: 'MIDDLE_FINGER_DIP', 
+        12: 'MIDDLE_FINGER_TIP', 
+        13: 'RING_FINGER_MCP', 
+        14: 'RING_FINGER_PIP', 
+        15: 'RING_FINGER_DIP', 
+        16: 'RING_FINGER_TIP', 
+        17: 'PINKY_MCP', 
+        18: 'PINKY_PIP', 
+        19: 'PINKY_DIP', 
+        20: 'PINKY_TIP'}
+
+        right_coord_dict = dict()
+        left_coord_dict = dict()
+
+        for key in id_dict:
+            if handedness == 'Right':
+                right_coord_dict[id_dict[key]] = (int(landmarks[key].x * frame.shape[1]), int(landmarks[key].y * frame.shape[0]))
+            elif handedness == 'Left':
+                left_coord_dict[id_dict[key]] = (int(landmarks[key].x * frame.shape[1]), int(landmarks[key].y * frame.shape[0]))
+            
+
+        return right_coord_dict, left_coord_dict
+
+
 
 if __name__ == '__main__':
     airMouse = AirMouse()
